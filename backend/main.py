c0ad6e025afbe9600d41fe79c8df01b43ac4c7e3
Vox-web <avotiyaaa@gmail.com>
2026-03-08 22:48:40 +0100

    Делаем распознавание текста_08.03.2026

--- backend/main.py
+++ backend/main.py
@@ -1313,7 +1313,7 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", "8080"))
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="0.0.0.0",  
         port=port,
         reload=True,
         log_level="info",
